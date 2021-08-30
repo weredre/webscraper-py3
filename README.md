f22a5f02e1ce6d403aa39a7df7c471bdce236565
Andre Heslop <andreheslop@gmail.com>
2021-08-30 14:03:52 -0400

    readme file updated

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# webscraper-py3
+# Coding Challenge
+### Built With
+* [Python Version 3.8.5](https://www.python.org/)
+* [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#)
+
+<!-- GETTING STARTED -->
+## Getting Started
+### Prerequisites
+
+python -m pip install beautifulsoup4
+
+<!-- USAGE EXAMPLES -->
+## Usage
+The program takes input parameters from the form, first_year and last_year variables. The output for the JSON is saved as a file data.json file. 
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import os
 import json
 from bs4 import BeautifulSoup
 
-form = "Form W-2"
-first_year = 2012
-last_year = 2021
-years_to_download = range(first_year, last_year+1, 1)
+form = "Form W-2 P"  # <-- input for form search
+first_year = 2012  # <-- input for first year for range pdf download
+last_year = 2021  # <-- input for last year for range pdf download
+years_to_download = range(first_year, last_year+1, 1)  # <-- range for pdf download
 URL = (
     "https://apps.irs.gov/app/picklist/list/priorFormPublication."
     "html?resultsPerPage=200&sortColumn=sortOrder&indexOfFirstRow=0&criteria=formNumber&value="
@@ -17,19 +17,19 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 min_year = 0
 max_year = 0
-i = 0
-for table_element in soup.select(".picklist-dataTable tr:has(td)"):  # <-- change the selector here
+
+for table_element in soup.select(".picklist-dataTable tr:has(td)"):  # <-- selecting only the table rows that have td
     form_number = table_element.find("td", class_="LeftCellSpacer")
     form_title = table_element.find("td", class_="MiddleCellSpacer")
     form_year = table_element.find("td", class_="EndCellSpacer")
-    if form_number.text.strip() == form:
+    if form_number.text.strip() == form:  # <-- if statement to match correct tax form
         data_form_number = form_number.text.strip()
         data_form_title = form_title.text.strip()
-    if int(form_year.text.strip()) > max_year:
+    if int(form_year.text.strip()) > max_year:  # <-- getting correct max year
         max_year = int(form_year.text.strip())
-    if int(form_year.text.strip()) < min_year or min_year == 0:
+    if int(form_year.text.strip()) < min_year or min_year == 0:   # <-- getting correct min year
         min_year = int(form_year.text.strip())
-    if form_number.text.strip() == form and int(form_year.text.strip()) in years_to_download:
+    if form_number.text.strip() == form and int(form_year.text.strip()) in years_to_download:   # <-- download pdf
         u = form_number.a["href"]
         p = "{}-{}.pdf".format(
             form_number.get_text(strip=True), form_year.get_text(strip=True)
